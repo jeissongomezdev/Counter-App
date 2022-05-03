330ee9f926cada0b676eba279391a8f899ed6403
Jeisson Gómez <jeissongomez@hotmail.es>
2022-05-03 12:51:46 -0500

    Eventos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import PrimeraApp from "./PrimeraApp";
+// import PrimeraApp from "./PrimeraApp";
 import CounterApp from "./CounterApp";
 import "./index.css";
 
