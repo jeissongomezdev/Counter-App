1859f5a86ce9e2c33211a6f7504b54bd2af4fb4f
Jeisson Gómez <jeissongomez@hotmail.es>
2022-05-03 12:23:10 -0500

    Ejercicio componente CounterApp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import PrimeraApp from "./PrimeraApp";
+import CounterApp from "./CounterApp";
 import "./index.css";
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(<PrimeraApp saludo="Hola Mundo" />);
+root.render(<CounterApp value={29} />);
